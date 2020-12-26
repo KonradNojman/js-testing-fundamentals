c4aac36376ec2f9fc5cf308e360a6f9d0d4421f3
larto <94konrad.n@gmail.com>
2020-12-26 22:29:47 +0100

    lesson 3 done

--- lessons/testing-framework.js
+++ lessons/testing-framework.js
@@ -16,19 +16,46 @@
  * Execute: Use `node lessons/testing-framework.js` to run the test.
  */
 
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require("../math");
 
-test('sum adds numbers', () => {
-  const result = sum(3, 7)
-  const expected = 10
-  expect(result).toBe(expected)
-})
+test("sum adds numbers", () => {
+	const result = sum(3, 7);
+	const expected = 10;
+	expect(result).toBe(expected);
+});
 
-test('subtract subtracts numbers', () => {
-  const result = subtract(7, 3)
-  const expected = 4
-  expect(result).toBe(expected)
-})
+test("subtract subtracts numbers", () => {
+	const result = subtract(7, 3);
+	const expected = 4;
+	expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+	try {
+		callback();
+		console.log(`✔ ${title}`);
+	} catch (error) {
+		console.log(`❌ ${title}`, error);
+	}
+}
+
+function expect(actual) {
+	return {
+		toBe: (expected) => {
+			if (actual !== expected)
+				throw new Error(`${actual} not equal to ${expected}`);
+		},
+		toEqual: (expected) => {},
+		toBeGreaterThan: (expected) => {
+			if (!(actual > expected))
+				throw new Error(`${actual} not greater than ${expected}`);
+		},
+		toBeLessThan: (expected) => {
+			if (!(actual < expected))
+				throw new Error(`${actual} not less than ${expected}`);
+		},
+	};
+}
 
 /**
  * Answer: Checkout the master branch for the answer.
