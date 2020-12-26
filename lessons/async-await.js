f41d91a45782bc3c8febdc0f9b4b73be6f081d98
larto <94konrad.n@gmail.com>
2020-12-26 22:35:20 +0100

    lesson 4->5 done

--- lessons/async-await.js
+++ lessons/async-await.js
@@ -15,20 +15,46 @@
  * Execute: Use `node lessons/async-await.js` to run the test.
  */
 
-const {sumAsync, subtractAsync} = require('../math')
+const { sumAsync, subtractAsync } = require("../math");
 
-test('sumAsync adds numbers asynchronously', () => {
-  const result = sumAsync(3, 7)
-  const expected = 10
-  expect(result).toBe(expected)
-})
+test("sumAsync adds numbers asynchronously", async () => {
+	const result = await sumAsync(3, 7);
+	const expected = 10;
+	expect(result).toBe(expected);
+});
 
-test('subtractAsync subtracts numbers asynchronously', () => {
-  const result = subtractAsync(7, 3)
-  const expected = 4
-  expect(result).toBe(expected)
-})
+test("subtractAsync subtracts numbers asynchronously", async () => {
+	const result = await subtractAsync(7, 3);
+	const expected = 4;
+	expect(result).toBe(expected);
+});
 
+async function test(title, callback) {
+	try {
+		await callback();
+		console.log(`✔ ${title}`);
+	} catch (error) {
+		console.log(`❌ ${title}`, error);
+	}
+}
+
+function expect(actual) {
+	return {
+		toBe: (expected) => {
+			if (actual !== expected)
+				throw new Error(`${actual} not equal to ${expected}`);
+		},
+		toEqual: (expected) => {},
+		toBeGreaterThan: (expected) => {
+			if (!(actual > expected))
+				throw new Error(`${actual} not greater than ${expected}`);
+		},
+		toBeLessThan: (expected) => {
+			if (!(actual < expected))
+				throw new Error(`${actual} not less than ${expected}`);
+		},
+	};
+}
 /**
  * Answer: Checkout the master branch for the answer.
  */
