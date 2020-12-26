866f1c1d189921de672185851b557af36bde28fe
larto <94konrad.n@gmail.com>
2020-12-26 22:22:31 +0100

    lesson 2 done

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -16,17 +16,35 @@
  *
  * Execute: Use `node lessons/assertion-library.js` to run the test.
  */
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require("../math");
 
-let result, expected
+let result, expected;
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+result = sum(3, 7);
+expected = 10;
+expect(result).toBe(expected);
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+result = subtract(7, 3);
+expected = 4;
+expect(result).toBe(expected);
+
+function expect(actual) {
+	return {
+		toBe: (expected) => {
+			if (actual !== expected)
+				throw new Error(`${actual} not equal to ${expected}`);
+		},
+		toEqual: (expected) => {},
+		toBeGreaterThan: (expected) => {
+			if (!(actual > expected))
+				throw new Error(`${actual} not greater than ${expected}`);
+		},
+		toBeLessThan: (expected) => {
+			if (!(actual < expected))
+				throw new Error(`${actual} not less than ${expected}`);
+		},
+	};
+}
 
 /**
  * Answer: Checkout the master branch for the answer.
